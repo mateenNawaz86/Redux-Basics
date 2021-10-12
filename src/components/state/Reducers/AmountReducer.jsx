3a036bc07042229aa46be5db7d0e276498a6dd89
Mateen Mirani <mateenmirani125@gmail.com>
2021-10-12 21:51:41 +0500

    setting up actionCreator & reducers

---
+++ src/components/state/Reducers/AmountReducer.jsx
@@ -0,0 +1,11 @@
+export default reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "deposite":
+      return state + action.payload;
+    case "withdraw":
+      return state - action.payload;
+
+    default:
+      return state;
+  }
+};
